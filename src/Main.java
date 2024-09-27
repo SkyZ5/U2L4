ea36f7fb83d53adb01bbdfa1f5240bcf9f21f56e
SkyZ5 <kmei0013@yahoo.com>
2024-09-27 16:30:14 -0400

    Updated version of TipCalculator
    Remade the variables and the methods so that it actually works as a program. Before it was basically just the original code copy and pasted. Now it functions as a program.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         TipCalculator tipcalculator = new TipCalculator();
+        Scanner s = new Scanner(System.in);
+
         tipcalculator.greeting();
-        tipcalculator.output();
+        
+        System.out.print("What is your total bill?: ");
+        String bill = s.nextLine();
+        double totalBill = Double.parseDouble(bill);
+        System.out.print("What is your tip percentage?: ");
+        String percentage = s.nextLine();
+        double tipPercentage = Double.parseDouble(percentage);
+        System.out.print("How many people are taking the bill?: ");
+        String people = s.nextLine();
+        int numberOfPeople = Integer.parseInt(people);
+        System.out.println("----------------------------------------------");
+
+        TipCalculator tipCalculator2 = new TipCalculator(totalBill, tipPercentage, numberOfPeople);
+        tipCalculator2.output();
 
     }
 }
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,54 +1,38 @@
-import java.util.Scanner;
-
 public class TipCalculator {
 
-    private double totalBill;
-    private double tipPercentage;
-    private int numberOfPeople;
+    private double tipPerPerson;
+    private double totalPerPerson;
+    String totalTipAmountConverted;
+    String totalBillWithTipConverted;
+    String tipPerPersonConverted;
+    String totalPerPersonConverted;
 
 
     public TipCalculator() {
     }
-
-    public void greeting() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("----------------------------------------------");
-        System.out.println("This program only accepts numbers (No symbols)");
-        System.out.println("----------------------------------------------");
-        System.out.print("What is your total bill?: ");
-        String bill = s.nextLine();
-        this.totalBill = Double.parseDouble(bill);
-        System.out.print("What is your tip percentage?: ");
-        String percentage = s.nextLine();
-        this.tipPercentage = Double.parseDouble(percentage);
-        System.out.print("How many people are taking the bill?: ");
-        String people = s.nextLine();
-        this.numberOfPeople = Integer.parseInt(people);
-        System.out.println("----------------------------------------------");
-    }
-
-    public void output() {
-        //CALCULATIONS
+    public TipCalculator(double totalBill, double tipPercentage, int numberOfPeople) {
         tipPercentage /= 100;
         double totalTipAmount = totalBill * tipPercentage;
         double totalBillWithTip = totalBill + totalTipAmount;
-        double tipPerPerson = totalTipAmount / numberOfPeople;
-        double totalPerPerson = totalBillWithTip / numberOfPeople;
+        tipPerPerson = totalTipAmount / numberOfPeople;
+        totalPerPerson = totalBillWithTip / numberOfPeople;
+        totalTipAmountConverted = convert(totalTipAmount);
+        totalBillWithTipConverted = convert(totalBillWithTip);
+        tipPerPersonConverted = convert(tipPerPerson);
+        totalPerPersonConverted = convert(totalPerPerson);
+    }
+    public void greeting() {
+        System.out.println("----------------------------------------------");
+        System.out.println("This program only accepts numbers (No symbols)");
+        System.out.println("----------------------------------------------");
+    }
+    
+    public String convert(double value) { 
+        double rounded = Math.round(value * 100.0) / 100.0;
+        return String.format("%.2f", rounded);
+    }
 
-        //Rounding
-        double totalTipAmountRounded = Math.round(totalTipAmount * 100.0) / 100.0;
-        double totalBillWithTipRounded = Math.round(totalBillWithTip * 100.0) / 100.0;
-        double tipPerPersonRounded = Math.round(tipPerPerson * 100.0) / 100.0;
-        double totalPerPersonRounded = Math.round(totalPerPerson * 100.0) / 100.0;
-
-
-        //Formating
-        String totalTipAmountConverted = String.format("%.2f", totalTipAmountRounded);
-        String totalBillWithTipConverted = String.format("%.2f", totalBillWithTipRounded);
-        String tipPerPersonConverted = String.format("%.2f", tipPerPersonRounded);
-        String totalPerPersonConverted = String.format("%.2f", totalPerPersonRounded);
-
-        //OUTPUT
+    public void output() {
         System.out.println("Total amount tipped: $" + totalTipAmountConverted);
         System.out.println("Total bill including tip: $" + totalBillWithTipConverted);
         System.out.println("Tip per person: $" + tipPerPersonConverted);
